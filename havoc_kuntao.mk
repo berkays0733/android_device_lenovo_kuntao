c5996787b8f159908b1cfb6dc1ff4f0e73077be4
Murat Kozan <muratkozan350@gmail.com>
2021-02-01 08:59:02 +0300

    kuntao: Enable updating of apex

--- havoc_kuntao.mk
+++ havoc_kuntao.mk
@@ -24,6 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit some common Havoc stuff.
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
